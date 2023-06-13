7d88b88e883809cb60fa60db9ef80d66a71083e9
Harshit Choudhary <harshitchoudhary18.hc@gmail.com>
2023-06-12 20:28:17 -0500

    Day-3 Striver's Sheet Challenge

---
+++ Day-3/merge_sorted_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, A: List[int], m: int, B: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a, b, write_index = m-1, n-1, m + n - 1
+
+        while b >= 0:
+            if a >= 0 and A[a] > B[b]:
+                A[write_index] = A[a]
+                a -= 1
+            else:
+                A[write_index] = B[b]
+                b -= 1
+
+            write_index -= 1
